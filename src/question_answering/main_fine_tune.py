723a19f30a097cc789ad81cf48968b6c7f972d7e
prepatdiscobj <i@rajul.net>
2024-01-15 00:52:08 +0530

    Added WandB callback to the trainer Updated requirements.txt with wandb dependency Removed dataclass for trainer arguments Added command line arguments for reading training arguments Removed text files for storing terminal output

--- src/question_answering/main_fine_tune.py
+++ src/question_answering/main_fine_tune.py
@@ -1,37 +1,13 @@
-import nltk
+from functools import partial
+
+import click
 import evaluate
+import nltk
 import numpy as np
 from datasets import load_dataset
-from transformers import T5Tokenizer, DataCollatorForSeq2Seq
 from transformers import T5ForConditionalGeneration, Seq2SeqTrainingArguments, Seq2SeqTrainer
-from functools import partial
-from dataclasses import dataclass
-from util.python_util import get_latest_file_name, setup_output_loggin
-
-
-@dataclass
-class TrainingArgs:
-    eval_batch_size: int = 4
-    learning_rate: float = 1e-4,
-    num_epochs: int = 3,
-    output_dir: str = "./results"
-    save_total_limit: int = 3,
-    train_batch_size: int = 8,
-    weight_decay: float = 1e-2
-
-    def create_training_arguments(self):
-        return Seq2SeqTrainingArguments(
-            evaluation_strategy="epoch",
-            learning_rate=self.learning_rate,
-            num_train_epochs=self.num_epochs,
-            output_dir=self.output_dir,
-            per_device_eval_batch_size=self.eval_batch_size,
-            per_device_train_batch_size=self.train_batch_size,
-            predict_with_generate=True,
-            push_to_hub=False,
-            save_total_limit=self.save_total_limit,
-            weight_decay=self.weight_decay,
-        )
+from transformers import T5Tokenizer, DataCollatorForSeq2Seq
+from transformers.integrations import WandbCallback
 
 
 def load_model_with_tokenizer(model_name):
@@ -80,7 +56,18 @@ def compute_metrics(eval_preds, tokenizer, metric_type="rouge"):
     return result
 
 
-def main():
+@click.argument("output_dir", type=str)
+@click.option("--lr", type=float, default=1e-4, help="Learning rate")
+@click.option("--epochs", type=int, default=3, help="Number of Epochs")
+@click.option("--eval_batch_size", type=int, default=4, help="Evaluation batch size")
+@click.option("--train_batch_size", type=int, default=8, help="Training batch size")
+@click.option("--num_checkpoints", type=int, default=3, help="Number of Checkpoints to retain")
+@click.option("--weight_decay", type=float, default=1e-2, help="Weight Decay for lr")
+def main(output_dir, lr, num_epochs, eval_batch_size, train_batch_size, num_checkpoints, weight_decay):
+    """
+    OUTPUT_DIR Output Directory where results will be stored
+
+    """
     # download punkt once only
     try:
         nltk.data.find('tokenizers/punkt')
@@ -88,9 +75,22 @@ def main():
         print('Downloading Punkt tokenizer')
         nltk.download("punkt", quiet=True)
 
-    print('Setting up Log files for capturing output and error')
-    log_filename = get_latest_file_name(__file__, "log")
-    err_filename = get_latest_file_name(__file__, "error")
+    training_args = Seq2SeqTrainingArguments(
+        evaluation_strategy="epoch",
+        learning_rate=lr,
+        num_train_epochs=num_epochs,
+        output_dir=output_dir,
+        per_device_eval_batch_size=eval_batch_size,
+        per_device_train_batch_size=train_batch_size,
+        predict_with_generate=True,
+        push_to_hub=False,
+        save_total_limit=num_checkpoints,
+        weight_decay=weight_decay,
+    )
+
+    # print('Setting up Log files for capturing output and error')
+    # log_filename = get_latest_file_name(__file__, "log")
+    # err_filename = get_latest_file_name(__file__, "error")
     # setup_output_loggin(err_filename, log_filename)
     print('Loading model .....')
     tokenizer, model = load_model_with_tokenizer("google/flan-t5-base")
@@ -103,8 +103,7 @@ def main():
     tokenize_validation = qa_dataset["validation"].map(preprocess_fn, batched=True)
 
     metric_fn = partial(compute_metrics, tokenizer=tokenizer)
-    args_obj = TrainingArgs()
-    training_args = args_obj.create_training_arguments()
+
     trainer = Seq2SeqTrainer(
         model=model,
         args=training_args,
@@ -112,7 +111,8 @@ def main():
         eval_dataset=tokenize_validation,
         tokenizer=tokenizer,
         data_collator=data_collator,
-        compute_metrics=metric_fn
+        compute_metrics=metric_fn,
+        callbacks=[WandbCallback()]
     )
     print('Everything ready!!! Preparing to train')
     trainer.train()
